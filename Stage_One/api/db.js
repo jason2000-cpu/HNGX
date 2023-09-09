6c4fc6f57ec5460f7d672e6d615f3dde20df841c
jason2000-cpu <jasonmwangi2000@gmail.com>
2023-09-10 00:28:07 +0300

    fixed errors:

--- Stage_One/api/db.js
+++ Stage_One/api/db.js
@@ -5,8 +5,8 @@ const week_days= ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Frida
 const db = {
     slack_name: "Backend_JM",
     current_day: week_days[current_time.getDay()],
-    utc_time: current_time.toUTCString(),
-    track: "Backend",
+    utc_time: current_time.toISOString(),
+    track: "backend",
     github_file_url: "https://github.com/jason2000-cpu/HNGX/blob/master/Stage_One/api/v1/index.js",
     github_repo_url: "https://github.com/jason2000-cpu/HNGX",
     status_code: 200
